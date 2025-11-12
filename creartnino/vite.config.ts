752d879a45f290d72891bdf642017b003fb3e48f
Miguel-Angel-Rincon <169084478+Miguel-Angel-Rincon@users.noreply.github.com>
2025-11-12 08:57:32 -0500

    vercel build configuration

--- creartnino/vite.config.ts
+++ creartnino/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/', // 👈 importante para producción
   build: {
     chunkSizeWarningLimit: 1600 // aumenta el límite en KB
   }
